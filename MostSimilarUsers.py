2d90307f5f50099c5f4d7c9f0e43f56048681e21
Camille Emig <camille.emig@gmail.com>
2019-11-30 14:03:02 -0500

    WOOOOOO

--- MostSimilarUsers.py
+++ MostSimilarUsers.py
@@ -1,7 +1,6 @@
 from operator import itemgetter
 from math import sqrt
-from PrepData import PrepData
-import time
+from MovieRatingData import MovieRatingData
 
 def find_cosine_similarity(ratings1, ratings2):
     d1 = 0
@@ -16,57 +15,67 @@ def find_cosine_similarity(ratings1, ratings2):
     return dot / (sqrt(d1)*sqrt(d2))
 
 
-def find_predicted_ratings_for_user(user_id):
-    data = PrepData()
-    data.prep_data()
+def find_predicted_ratings_for_data(user_1_data):
+    """
+    This method finds movies that other people who like the same stuff as you also like-
+    doesn't take into account if you have any genres/movies that are related to that movie
+    :param user_1_data:
+    :return:
+    """
+    data = MovieRatingData()
 
     sorted_users = sorted(data.users_to_ratings.keys())
-    test_users_movies = set(data.users_to_ratings[user_id].keys())
-    test_users_movies_with_ratings = data.users_to_ratings[user_id]
+    test_users_movies = set(user_1_data.keys())
+    test_users_movies_with_ratings = user_1_data
     # shared movies is stored as user_id -> set of shared movies
     shared_movies = {}
-    for user in sorted_users[1:]:
-        shared_movies[user] = test_users_movies & set(data.users_to_ratings[user].keys())
+    for user in sorted_users:
+        if len(test_users_movies) <= 30:
+            min_movies = len(test_users_movies)
+        else:
+            min_movies = 1
+        movies = test_users_movies & set(data.users_to_ratings[user].keys())
+        if movies and len(movies) >= min_movies:
+            shared_movies[user] = movies
 
     # similarities is stored as user_id -> user_similarity
     similarities = {}
-    for user in sorted_users[1:]:
-        if not shared_movies[user]:
-            continue
-        user1_movies = dict([(k, v) for k,v in test_users_movies_with_ratings.items() if k in shared_movies[user]])
-        user2_movies = dict([(k, v) for k,v in data.users_to_ratings[user].items() if k in shared_movies[user]])
+    for user, movies in shared_movies.items():
+        user1_movies = dict([(k, test_users_movies_with_ratings[k]) for k in movies])
+        user2_movies = dict([(k, data.users_to_ratings[user][k]) for k in movies])
         similarity = find_cosine_similarity(user1_movies, user2_movies)
         similarities[user] = similarity
 
-    # user averages is user_id -> average of all their ratings
+    # user averages is user_id -> average of all their movie_ratings
     user_averages = {}
     for user in sorted_users:
         ratings = list(data.users_to_ratings[user].values())
         average = sum(ratings)/len(ratings)
         user_averages[user] = average
 
-    # predicted ratings is movie name -> predicted rating
+    # predicted movie_ratings is movie name -> predicted rating
     predicted_ratings = {}
-    test_user_average = user_averages[user_id]
-    for movie in data.all_movies - test_users_movies:
+    test_ratings = list(test_users_movies_with_ratings.values())
+    test_user_average = sum(test_ratings) / len(test_ratings)
+    possible_movies = data.all_movies
+    for movie in possible_movies:
         total_sim = 0
         total_sim_rated = 0
-        for user in sorted_users[1:]:
-            if movie not in data.users_to_ratings[user] or user not in similarities:
+        for user in similarities:
+            if movie not in data.users_to_ratings[user]:
                 continue
             similarity = similarities[user]
             total_sim += similarity
             total_sim_rated += similarity*(data.users_to_ratings[user][movie] - user_averages[user])
         if total_sim == 0 or total_sim_rated == 0:
-            predicted_ratings[movie] = test_user_average
             continue
         predicted = total_sim_rated/total_sim
         predicted_ratings[movie] = test_user_average + predicted
     return sorted(predicted_ratings.items(), key=itemgetter(1), reverse=True)
 
 
-t0 = time.time()
-
-print(find_predicted_ratings_for_user(1))
-print('It took my system {:.2f}s to get top n movies \n\
-              '.format(time.time() - t0))
+## EX WHAT TO DO:
+data = MovieRatingData()
+test_users = list(data.TEST_DATA.keys())
+print(find_predicted_ratings_for_data(data.TEST_DATA[test_users[0]]))
+# compare those results to what's in data.TEST_DATA[test_users[0]]
